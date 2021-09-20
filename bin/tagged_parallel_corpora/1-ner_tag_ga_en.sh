a8db0c9015e8076b9d90aca4cec3ec4a56854480
Haukur Páll <haukurpalljonsson@gmail.com>
2021-09-20 11:39:05 +0000

    Parallel corpus scripts.

--- bin/tagged_parallel_corpora/1-ner_tag_ga_en.sh
+++ bin/tagged_parallel_corpora/1-ner_tag_ga_en.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=ner_parallel
-#SBATCH --output=ner_parallel.out
+#SBATCH --job-name=ner_tag_ga_en
 #SBATCH --gres=gpu:1
 OUT_DIR="/data/scratch/haukurpj/Projects/MT_NER_EVAL/parallel_corpora_out"
 echo $CUDA_VISIBLE_DEVICES
 mkdir -p $OUT_DIR
 
 DATASETS="greynir_articles_01-11-2020:01-06-2021 newscrawl_2007-2019"
+DATASETS="greynir_articles_01-11-2020:01-06-2021"
 LANGS="en is"
+LANGS="en"
 for lang in $LANGS; do
     for dataset in $DATASETS; do
         echo "Processing $dataset $lang"
         to_tag="$OUT_DIR/$dataset.$lang"
         unnorm_tags="$OUT_DIR/$dataset.$lang".unnorm
-        mt ner $to_tag $unnorm_tags --device cuda --batch_size 32 --lang $lang
+        mt ner $to_tag $unnorm_tags --device cuda --batch_size 64 --lang $lang
     done
 done
--- bin/tagged_parallel_corpora/3-filter.sh
+++ bin/tagged_parallel_corpora/3-filter.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 OUT_DIR="/data/scratch/haukurpj/Projects/MT_NER_EVAL/parallel_corpora_out"
 DATASETS="greynir_articles_01-11-2020:01-06-2021 newscrawl_2007-2019"
-LANGS="en is"
-    DATASET_NAME="$(basename $f)"
-    src_text=""
-    tgt_text=""
-    src_entities=""
-    tgt_entities=""
-    src_text_out=""
-    tgt_text_out=""
-    src_entities_out=""
-    tgt_entities_out=""
-    src_
-        tags="$OUT_DIR/$dataset.$lang".ner
-    mt filter-text-by-ner $f out/${DATASET_NAME}_entities out/${DATASET_NAME}_idxs out/${DATASET_NAME}_clean   
+TGT_LANG="is"
+SRC_LANG="en"
+for dataset in $DATASETS; do
+    src_text="$OUT_DIR/$dataset.$SRC_LANG"
+    tgt_text="$OUT_DIR/$dataset.$TGT_LANG"
+    src_entities="$OUT_DIR/$dataset.$SRC_LANG.unnorm"
+    tgt_entities="$OUT_DIR/$dataset.$TGT_LANG.unnorm"
+    src_text_out="$OUT_DIR/$dataset.filtered.$SRC_LANG"
+    tgt_text_out="$OUT_DIR/$dataset.filtered.$TGT_LANG"
+    src_entities_out="$OUT_DIR/$dataset.filtered.$SRC_LANG.ner"
+    tgt_entities_out="$OUT_DIR/$dataset.filtered.$TGT_LANG.ner"
+    mt filter-text-by-ner $src_text $tgt_text $src_entities $tgt_entities $src_text_out $tgt_text_out $src_entities_out $tgt_entities_out 
 done
